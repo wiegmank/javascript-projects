736e8413753dc26b21ed721a0dff1668a6703a20
Kevin Wiegman <kevin@Kevins-MacBook-Air.local>
2023-09-11 20:18:57 -0500

    added ';' like I should have

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -17,15 +17,15 @@ let milesToMars = distanceToMarsKM * milesPerKM
 
 let hoursToMars = milesToMars / shuttleSpeedMPH
 
-let daysToMars = hoursToMars / 24
+let daysToMars = hoursToMars / 24;
 
 // Print the results of the space mission calculations below
-console.log(shuttleName + " will take " + daysToMars + " to reach Mars.")
+console.log(shuttleName + " will take " + daysToMars + " to reach Mars.");
 
 // Calculate a trip to the moon below
-let milesToMoon = distanceToMoonKM * milesPerKM
-let hoursToMoon = milesToMoon / shuttleSpeedMPH
-let daysToMoon = hoursToMoon / 24
+let milesToMoon = distanceToMoonKM * milesPerKM;
+let hoursToMoon = milesToMoon / shuttleSpeedMPH;
+let daysToMoon = hoursToMoon / 24;
 
 // Print the results of the trip to the moon below
-console.log(shuttleName + " will take " + daysToMoon + " to reach the Moon.")
+console.log(shuttleName + " will take " + daysToMoon + " to reach the Moon.");
