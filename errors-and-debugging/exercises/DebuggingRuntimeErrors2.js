7a25271c08b23506edd5f85e96fe373fa1c99c91
Kevin Wiegman <kevin@Kevins-MacBook-Air.local>
2023-09-13 14:24:35 -0500

    code for DebuggingRuntimeErrors2.js copied from text

---
+++ errors-and-debugging/exercises/DebuggingRuntimeErrors2.js
@@ -0,0 +1,22 @@
+let launchReady = false;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (crewStatus && computerStatus === 'green') {
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}
+
+if (launchReady) {
+   console.log("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
+   console.log("Fed parrot...");
+   console.log("Ignition...");
+   console.log("Liftoff!");
+} else {
+   console.log("Launch scrubbed.");
+}
+
+//This code was copied directly from the text, and syntax errors were corrected - KW 9/13/23
