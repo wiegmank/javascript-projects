57d9d43cf36404616996d4181ba72e4cadb10e85
Kevin Wiegman <kevin@Kevins-MacBook-Air.local>
2023-10-11 19:00:49 -0500

    exceptions exercises complete

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -5,3 +5,9 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+function divide(numerator, denominator) {
+    if (denominator = 0) {
+        throw Error("Attempted to divide by zero.")
+    }
+    return (numerator/denominator);
+};
--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,10 +1,19 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
+
+  try {
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
+    } catch(err) {
+    console.log("Error thrown")
+    }
   }
 }
+
+/*Add a try/catch block inside of gradeLabs to catch 
+an exception if the runLab property is not defined. If the exception is thrown, result should 
+be set to the text "Error thrown".*/
 
 let studentLabs = [
   {
@@ -22,3 +31,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
