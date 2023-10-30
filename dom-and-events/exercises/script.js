3f453a1f73e2c4efc7bccb81cb96bbf3d614405a
Kevin Wiegman <kevin@Kevins-MacBook-Air.local>
2023-10-30 11:13:07 -0500

    dom exercise complete

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,24 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", function() {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover", function() {
+        missionAbort.style.background = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", function() {
+        missionAbort.style.background = '';
+    })
+
+    missionAbort.addEventListener("click", function() {
+        let response = window.confirm("Are you sure you want to abort the mission?");
+        if (response = true) {
+            paragraph.innerHTML = "Mission Aborted! Space Shuttle returning home";
+        };
+    });
 }
 
 window.addEventListener("load", init);
